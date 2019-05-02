773f46740465722bce456c7c69763b4a34375668
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-02 10:45:47 -0700

    feature 2 half way done

--- server.js
+++ server.js
@@ -15,21 +15,11 @@ app.get('/hello', (request, response) => {
 });
 
 const pgClient = new pg.Client(process.env.DATABASE_URL);
+pgClient.connect();
 
 app.get('/location', (request, response) => {
   const queryData = request.query.data;
-  let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GEOCODE_API_KEY}`;
-  superagent.get(geocodeURL)
-    .end((err, res) => {
-      if (err && err.status !== 200) {
-        const errorResponse500 = {'status': 500, 'responseText': 'Sorry, something went wrong' };
-
-        response.status(500).send(errorResponse500);
-      } else {
-        const location = new Location(queryData, res);
-        response.status(200).send(location);
-      }
-    });
+  checkLocationDB(queryData,response);
 });
 
 app.get('/weather', (request, response) => {
@@ -52,6 +42,7 @@ app.get('/weather', (request, response) => {
 app.get('/events', (request, response) => {
   const lat = request.query.data.latitude;
   const lng = request.query.data.longitude;
+
   const eventURL =`https://www.eventbriteapi.com/v3/events/search?location.longitude=${lng}&location.latitude=${lat}&expand=venue&token=${process.env.EVENTBRITE_API_KEY}`;
   superagent.get(eventURL)
     .end((err, res) => {
@@ -72,9 +63,9 @@ app.use('*', (request, response) => response.send('Sorry, that route does not ex
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
 
 const Location = function(searchQuery, jsonData) {
-  const formattedQuery = jsonData.body['results'][0]['formatted_address'];
-  const latitude = jsonData.body['results'][0]['geometry']['location']['lat'];
-  const longitude = jsonData.body['results'][0]['geometry']['location']['lng'];
+  const formattedQuery = jsonData['formatted_address'];
+  const latitude = jsonData['geometry']['location']['lat'];
+  const longitude = jsonData['geometry']['location']['lng'];
 
   this.search_query = searchQuery;
   this.formatted_query = formattedQuery;
@@ -106,5 +97,30 @@ const Event = function(jsonData) {
       'event_date': event_date,
       'summary': summary
     };
+  });
+};
+
+const checkLocationDB = function(queryData,response){
+  const sqlStatement = 'SELECT * FROM location WHERE search_query = $1';
+  const values = [ queryData ];
+  return pgClient.query(sqlStatement,values).then((data) => {
+    if(data.rowCount) {
+      return response.status(200).send(data.rows[0]);
+    } else {
+      let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GEOCODE_API_KEY}`;
+      superagent.get(geocodeURL)
+        .end((err, res) => {
+          if (err && err.status !== 200) {
+            const errorResponse500 = {'status': 500, 'responseText': 'Sorry, something went wrong' };
+            return response.status(500).send(errorResponse500);
+          } else {
+            let location = new Location(queryData, res.body.results[0]);
+            const sqlInsert = 'INSERT INTO location (latitude, longitude, formatted_query, search_query) VALUES ($1, $2, $3, $4)';
+            const args = [ location.latitude, location.longitude, location.formatted_query, location.search_query];
+            pgClient.query(sqlInsert, args);
+            return response.status(200).send(location);
+          }
+        });
+    }
   });
 };
